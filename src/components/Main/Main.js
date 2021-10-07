77c7dd2c2e89029368fa3c069fc94f88bea52a14
Tanya Komar <tatiana23komar@gmail.com>
2021-10-07 10:27:26 -0700

    add CardList, Card

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import './Main.css';
 import Map from '../Map/Map';
+import CardList from '../CardList/CardList';
 
 function Main () {
     return (
         <main>
             <Map />
+            <CardList />
         </main>
     )
 }
