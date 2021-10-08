86765ab327068cb5ab8cf5442b594d309dfdb800
Sara Ward <saro4cka.ward@yandex.com>
2021-10-08 03:37:43 +0100

    updating layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,11 @@ function Footer() {
 
       <div className="footer__row">
 
-        <img className="footer_logo" src="images/footer_logo.svg" alt="Travel Able text logo"></img>
+        <div className="footer__column">
+          <img className="footer_logo" src="images/footer_logo.svg" alt="Travel Able text logo"></img>
+          <p className="footer__copyright">&copy; 2021 travelable</p>
+          <a href="" target="_blank"></a>
+        </div>
 
         <div className="footer__input">
           <div className="footer__input_email">
@@ -15,6 +19,20 @@ function Footer() {
               className="button" target="_blank">Subscribe</a>
           </div>
           <p className="footer__input_text">Subscribe and you will be the first to know about changes</p>
+
+          <ul className="footer__menu">
+
+            <li className="footer__terms">terms of service</li>
+            <a href="" target="_blank">
+              <li className="footer__privacy">privacy policy</li>
+            </a>
+            <a href="" target="_blank">
+              <li className="footer__covid">Covid-19</li>
+            </a>
+            <a href="" target="_blank">
+              <li className="footer__contact">contact us</li>
+            </a>
+          </ul>
         </div>
 
 
@@ -32,21 +50,7 @@ function Footer() {
       </div>
 
 
-      <ul className="footer__menu">
-        <li className="footer__copyright">&copy; 2021 travelable</li>
-        <a href="" target="_blank">
-          <li className="footer__terms">terms of service</li>
-        </a>
-        <a href="" target="_blank">
-          <li className="footer__privacy">privacy policy</li>
-        </a>
-        <a href="" target="_blank">
-          <li className="footer__covid">Covid-19</li>
-        </a>
-        <a href="" target="_blank">
-          <li className="footer__contact">contact us</li>
-        </a>
-      </ul>
+
 
     </footer>
   )
