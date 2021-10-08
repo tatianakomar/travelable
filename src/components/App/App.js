4b49522bb8e7089e87bbabdd229a23d9721e97f6
Tanya Komar <tatiana23komar@gmail.com>
2021-10-07 19:45:34 -0700

    Fix header merge conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-// import Header from '../Header/Header';
+import Header from '../Header/Header';
 import Main from '../Main/Main';
 // import Footer from '../Footer/Footer'; 
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Main />
       {/* <Footer /> */}
     </>
