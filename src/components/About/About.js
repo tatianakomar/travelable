eb04169e40e1a27fcb28bfddebf3e6098ac651cb
Sara Ward <saro4cka.ward@yandex.com>
2021-10-08 01:24:03 +0100

    implement footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,10 @@
 import './About.css';
 
-function About () {
+function About() {
     return (
         <section className="about">
             <div className="about__text">
-                <h2 className="about__title">About us</h2>
-                <p className="about__description">We know how it’s difficult to plan a vacation in covid-world. So we tried to do it easier for you. Just choose some filters which are important for you and see which states you can visit according your filters.</p>
+                <h2 className="about__title"></h2>
             </div>
         </section>
     )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,75 @@
 import './Footer.css';
 
-function Footer () {
-    return (
-        <footer className="footer">
-            <div className="footer__content">
-                <ul className="footer__contacts">
-                    <li className="footer__contact"></li>
-                </ul>
-                <p className="footer__copyright">© Travelable, 2021</p>
-            </div>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <nav className="footer__column footer__column__list">
+          <ul className="footer__row">
+            <li>
+              <img className="footer_logo" src="images/footer_logo.svg" alt="Travel Able text logo"></img>
+            </li>
+
+            <li className="footer__input">
+              <input type="email" id="email" name="email" placeholder="email"></input>
+            </li>
+            <li>
+              <a href=""
+                class="button" target="_blank">subscribe</a>
+            </li>
+
+
+
+
+
+            <ul className="footer__apps">
+              <li>
+                <a href="" class="footer__column-link" target="_blank">
+                  <img src="images/app_store.svg" class="footer__app-icons"
+                    alt="Apple App Store icon, white outline of an apple"></img>
+                </a>
+              </li>
+              <li>
+                <a href="" class="footer__column-link" target="_blank">
+                  <img src="images/google_play.svg" class="footer__app-icons"
+                    alt="Google Play background"></img>
+                </a>
+              </li>
+            </ul>
+          </ul>
+        </nav>
+
+
+
+
+
+        <ul className="footer__menu">
+          <li className="footer__copyright">&copy; 2021 travelable</li>
+          <a href="" target="_blank">
+            <li className="footer__terms">terms of service</li>
+          </a>
+          <a href="" target="_blank">
+            <li className="footer__privacy">privacy policy</li>
+          </a>
+          <a href="" target="_blank">
+            <li className="footer__covid">Covid-19</li>
+          </a>
+          <a href="" target="_blank">
+            <li className="footer__contact">contact us</li>
+          </a>
+        </ul>
+
+
+
+
+        <nav class="footer__column footer__column_content_apps">
+          <ul class="footer__list">
+
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer;
